1160e42c6e4524a51da464a66e90266c87b91d22
Zou Zhipeng <495043143@qq.com>
2020-05-29 10:45:36 +0800

    mongodb

--- extensions/php72.sh
+++ extensions/php72.sh
@@ -77,11 +77,11 @@ fi
 
 if [ -z "${EXTENSIONS##*,mongodb,*}" ]; then
     echo "---------- Install mongodb ----------"
-	# apk add --no-cache unixodbc-dev
-    # printf "\n" | pecl install mongodb
-    # docker-php-ext-enable mongodb
-    pear install mongodb-1.7.4.tgz
+	apk add --no-cache unixodbc-dev
+    printf "\n" | pecl install mongodb
     docker-php-ext-enable mongodb
+    # pear install mongodb-1.7.4.tgz
+    # docker-php-ext-enable mongodb
 fi
 
 if [ -z "${EXTENSIONS##*,tideways,*}" ]; then
